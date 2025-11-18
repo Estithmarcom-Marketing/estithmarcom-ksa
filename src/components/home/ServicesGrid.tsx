01451a65cc8366440bdebbb53d66ab69cef570c4
amr ibrahim <amramr3434@gmail.com>
2025-11-18 14:11:25 +0200

    videos and service in home

--- src/components/home/ServicesGrid.tsx
+++ src/components/home/ServicesGrid.tsx
@@ -4,9 +4,19 @@ import ServiceCard from "@/components/shared/ServiceCard";
 const ServicesGrid = () => {
   const services = [
     {
+      icon: Rocket,
+      titleKey: "service.incubation.title",
+      descriptionKey: "service.incubation.description"
+    },
+    {
       icon: Scale,
       titleKey: "service.legal.title",
       descriptionKey: "service.legal.description"
+    },
+    {
+      icon: Briefcase,
+      titleKey: "service.government.title",
+      descriptionKey: "service.government.description"
     },
     {
       icon: Building2,
@@ -14,24 +24,14 @@ const ServicesGrid = () => {
       descriptionKey: "service.company.description"
     },
     {
-      icon: Rocket,
-      titleKey: "service.incubation.title",
-      descriptionKey: "service.incubation.description"
-    },
-    {
-      icon: Home,
-      titleKey: "service.workspace.title",
-      descriptionKey: "service.workspace.description"
-    },
-    {
       icon: FileText,
       titleKey: "service.residency.title",
       descriptionKey: "service.residency.description"
     },
     {
-      icon: Briefcase,
-      titleKey: "service.government.title",
-      descriptionKey: "service.government.description"
+      icon: Home,
+      titleKey: "service.workspace.title",
+      descriptionKey: "service.workspace.description"
     }
   ];
 
