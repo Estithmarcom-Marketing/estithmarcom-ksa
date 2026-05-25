75d4798becbafb3a854d1cbd38a3e345fff58b9e
MahmoudMohamed26 <mahmoudkamel26300@gmail.com>
2026-05-25 11:17:38 +0300

    Update: Redirect of free-zones in proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -15,12 +15,19 @@ export async function proxy(request: NextRequest) {
   }
 
   const pathnameHasLocale = SUPPORTED_LOCALES.some(
-    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
   );
 
   if (pathnameHasLocale) {
     const response = NextResponse.next();
     const currentLocale = pathname.split("/")[1];
+    const pathWithoutLocale = pathname.slice(`/${currentLocale}`.length);
+
+    if (pathWithoutLocale === "/free-zones") {
+      const url = request.nextUrl.clone();
+      url.pathname = `/${currentLocale}`;
+      return NextResponse.redirect(url);
+    }
     response.headers.set("x-locale", currentLocale);
     response.headers.set("x-pathname", pathname);
     return response;
@@ -28,7 +35,7 @@ export async function proxy(request: NextRequest) {
 
   const locale =
     SUPPORTED_LOCALES.find(
-      (l) => l === request.cookies.get("mithaq_lang")?.value
+      (l) => l === request.cookies.get("mithaq_lang")?.value,
     ) ?? DEFAULT_LOCALE;
 
   const url = request.nextUrl.clone();
