9166a1bf554369d35f59626e52367e65b57569d1
MahmoudMohamed26 <mahmoudkamel26300@gmail.com>
2026-06-08 17:58:40 +0300

    Update: About-us page with metadata

--- src/app/[locale]/(website)/about-us/page.tsx
+++ src/app/[locale]/(website)/about-us/page.tsx
@@ -1,5 +1,22 @@
 import { getHighlights } from "@/lib/apis/stats";
 import AboutUsClient from "./_components/abous-us-client";
+import { Metadata } from "next";
+import { getTranslator, Locale } from "@/lib/i18n";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: Locale }>;
+}): Promise<Metadata> {
+  const { locale } = await params;
+  const { t } = getTranslator(locale);
+
+  return {
+    title: t("aboutus.meta.title"),
+    description: t("aboutus.meta.description"),
+    keywords: t("aboutus.meta.keywords"),
+  };
+}
 
 export default async function AboutUsPage() {
   const stats = await getHighlights();
