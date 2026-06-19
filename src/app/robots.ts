5eb3f4818a5f844086068b2ee359e3dae6f3944f
MahmoudMohamed26 <mahmoudkamel26300@gmail.com>
2026-06-19 16:23:35 +0300

    Update: robots.ts baseUrl with production

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://test.estithmarcom.com";
+  const baseUrl = "https://estithmarcom.com";
   return {
     rules: {
       userAgent: "*",
