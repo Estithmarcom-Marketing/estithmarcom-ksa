d62941d354047ae3827218543ea96d1b7cb7c3db
MahmoudMohamed26 <mahmoudkamel26300@gmail.com>
2026-07-14 15:28:46 +0300

    Update: Service-item style

--- src/components/service/service-item.tsx
+++ src/components/service/service-item.tsx
@@ -11,24 +11,34 @@ export default function ServiceItem({ service }: { service: ServiceType }) {
   const locale = useLocale();
   const { t } = getTranslator(locale);
   return (
-    <div className="rounded-2xl aspect-[387/447] relative overflow-hidden">
-      <div className="absolute w-full h-full">
-        {service.image ? <Image
-          src={service.image}
-          alt={service.title}
-          fill
-          className="object-cover"
-        /> : <div className="absoulte w-full h-full top-0 left-0 bg-black/90"></div>}
+    <div className="rounded-2xl bg-[#f2f3f2] relative overflow-hidden">
+      <div className="w-[99px] h-[99px] m-auto mt-10 relative">
+        {service.image ? (
+          <Image
+            src={service.image}
+            alt={service.title}
+            fill
+            className="object-cover"
+          />
+        ) : (
+          <div className="absoulte w-full h-full top-0 left-0 bg-black/90"></div>
+        )}
       </div>
-      <div className="absolute bottom-0 py-5 px-10 text-center text-white w-full">
+      <div className="py-5 px-5 text-center text-black w-full">
         <h2 className="mt-2 font-bold mb-4">{service.title}</h2>
-        <p className="text-sm text-white/90">{service.short_description}</p>
-        <div className="flex justify-center mt-4">
+        <p className="text-sm text-black/90 px-5">{service.short_description}</p>
+        <div className="grid grid-cols-3 gap-2 mt-6">
           <Link
             href={`/services/${service.slug}`}
-            className="text-secondary hover:underline"
+            className="py-1 col-span-2 bg-primary text-white rounded-full font-bold hover:bg-primary/90 duration-300"
           >
-            <CircleArrowLeft size={33} className="ltr:rotate-180 duration-300 hover:text-white" />
+            {t("requestService")}
+          </Link>
+          <Link
+            href={`/services/${service.slug}`}
+            className="py-1 bg-secondary text-white rounded-full font-bold hover:bg-secondary/90 duration-300"
+          >
+            {t("readmore")}
           </Link>
         </div>
       </div>
