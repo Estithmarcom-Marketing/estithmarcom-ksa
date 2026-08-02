36b1fc1570687cb8568e5b3faa93541ec30ef421
MahmoudMohamed26 <mahmoudkamel26300@gmail.com>
2026-08-02 15:07:44 +0300

    Refactor: Update styling for PartnerItem and CarouselItem components

--- src/components/partners/partner-item.tsx
+++ src/components/partners/partner-item.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 export default function PartnerItem({ partner }: { partner: PartnerType }) {
   return (
-    <div className="border rounded-md py-5 px-8">
-      <Link href={partner.link} className="aspect-[200/110] block select-none relative">
+    <div className="rounded-md px-4">
+      <Link href={partner.link} className="aspect-square block select-none relative">
         <Image
           src={partner.image}
           fill
